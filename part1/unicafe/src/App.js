f12042194cd872185302f06438f76053b0a31be2
Arpit <arpit.rai@viayam.today>
2021-03-14 16:54:22 +0530

    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,11 +16,22 @@ const Statistics = (props) => {
     );
 }
 
+const Button = (props) => {
+    return (
+        <button onClick = {props.handleClick}>{props.text}</button>
+    );
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good+1);
+  const handleNeutralClick = () => setNeutral(neutral+1);
+  const handleBadClick = () => setBad(bad+1);
 
   const all = bad+good+neutral;
   const avg = (good - bad)/all;
@@ -29,9 +40,9 @@ const App = () => {
     <div>
       <h1>Give Feedback</h1>
       <div>
-          <button onClick={() => {setGood(good+1)}}> good </button>
-          <button onClick={() => {setNeutral(neutral+1)}}> neutral </button>
-          <button onClick={() => {setBad(bad+1)}}> bad </button>
+          <Button text='good' handleClick={handleGoodClick}></Button>
+          <Button text='neutral' handleClick={handleNeutralClick}></Button>
+          <Button text='bad' handleClick={handleBadClick}></Button>
       </div>
       <br></br>
       <h1>Statistics </h1> 
