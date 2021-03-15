59e5611228622e1a3a1d8930808ac280989f8eb4
Arpit <arpit.rai@viayam.today>
2021-03-15 13:53:44 +0530

    1.12*: anecdotes step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,59 +1,25 @@
 import React, { useState } from 'react'
 
-const Statistics = (props) => {
-
-    if( props.all === 0 )
-        return <div>No feedback given </div>
-    return (
-        <div>
-        <Statistic text='good' value={props.good} ></Statistic>
-        <Statistic text='neutral' value={props.neutral} ></Statistic>
-        <Statistic text='bad' value={props.bad} ></Statistic>
-        <Statistic text='all' value={props.all} ></Statistic>
-        <Statistic text='average' value={props.avg} ></Statistic>
-        <Statistic text='positive' value={props.posPer} ></Statistic>
-        </div>
-    );
-}
-
-const Statistic = (props) => {
-    return(
-        <div>{props.text} {props.value}</div>
-    )
-}
-
-const Button = (props) => {
-    return (
-        <button onClick = {props.handleClick}>{props.text}</button>
-    );
-}
-
-
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
 
-  const handleGoodClick = () => setGood(good+1);
-  const handleNeutralClick = () => setNeutral(neutral+1);
-  const handleBadClick = () => setBad(bad+1);
-
-  const all = bad+good+neutral;
-  const avg = (good - bad)/all;
-  const posPer = (good/all)*100;
+  const [anec,setAnec] = useState(0);
+  const len = anecdotes.length;
+  const val = (anec+1)%(len);
+  console.log(len);
   return (
-    <div>
-      <h1>Give Feedback</h1>
       <div>
-          <Button text='good' handleClick={handleGoodClick}></Button>
-          <Button text='neutral' handleClick={handleNeutralClick}></Button>
-          <Button text='bad' handleClick={handleBadClick}></Button>
+          <h4>{anecdotes[anec]}</h4>
+          <button onClick={()=>setAnec(val)}>Next Anecdote</button>
       </div>
-      <br></br>
-      <h1>Statistic </h1> 
-     <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} posPer={posPer}></Statistics>
-    </div>
   )
 }
 
