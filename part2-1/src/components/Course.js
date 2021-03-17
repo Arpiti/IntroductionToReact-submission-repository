05ad63159c2ead7fca6aabd7897d81700c33ed38
Arpit <arpit.rai@viayam.today>
2021-03-17 18:35:00 +0530

    2.2: Course information step7

--- part2-1/src/components/Course.js
+++ part2-1/src/components/Course.js
@@ -2,7 +2,7 @@ const Content = ({parts}) => {
     return (
         <ul>
             {parts.map((part)=>{
-                return ( <li> {part.name} {part.exercises}</li> );
+                return ( <li key={part.id}> {part.name} {part.exercises}</li> );
             })}
         </ul>
     );
@@ -10,12 +10,17 @@ const Content = ({parts}) => {
 
 const Course = (props) => {
   //  console.log(props.course.parts);
+  const totSum = props.course.parts.reduce(function(sum,part){
+    return sum = sum + part.exercises;
+},0);
+
     return (
         
         <div>
             <h1> {props.course.name}</h1>    
             <br></br>
             <Content parts={props.course.parts}></Content>
+            <strong> total of {totSum} exercises </strong>
         </div>
     )
 }
