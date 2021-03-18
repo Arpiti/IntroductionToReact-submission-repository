439ac5a9f515703e32935e0bfd4cb5f242055021
Arpit <arpit.rai@viayam.today>
2021-03-18 14:20:43 +0530

    2.4: Course information step9

--- part2-1/src/components/Course.js
+++ part2-1/src/components/Course.js
@@ -8,19 +8,35 @@ const Content = ({parts}) => {
     );
 }
 
-const Course = (props) => {
-  //  console.log(props.course.parts);
-  const totSum = props.course.parts.reduce(function(sum,part){
-    return sum = sum + part.exercises;
-},0);
+const Heading = ({name}) => {
+    return (
+        <div>
+        <h1>{name}</h1>
+        <br></br>
+        </div>
+    )
+}
+
+const Course = ({courses}) => {
+
+    //  console.log(props.course.parts);
+  
 
     return (
         
         <div>
-            <h1> {props.course.name}</h1>    
-            <br></br>
-            <Content parts={props.course.parts}></Content>
-            <strong> total of {totSum} exercises </strong>
+            {courses.map((course)=>{
+                 const totSum = course.parts.reduce((sum,part) =>  sum = sum + part.exercises ,0);
+                return (
+                   
+                    <div key={course.id}>
+                        <Heading name={course.name}></Heading>
+                        <Content parts={course.parts}></Content>
+                        <strong> total of {totSum} exercises </strong>
+                    </div>
+             )
+            })} 
+           
         </div>
     )
 }
