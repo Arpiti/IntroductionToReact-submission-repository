814b6ba14281e9db848c25e2d88b3b9efcc6680c
Arpit <arpit.rai@viayam.today>
2021-03-18 16:53:32 +0530

    part2-2 branch

--- part2-1/src/App.js
+++ part2-1/src/App.js
@@ -1,53 +1,47 @@
-import './App.css';
-import Course  from './components/Course';
+import React, { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const App = (props) => {
+  
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...') 
+
+  const addNote = (event) => {   
+     event.preventDefault();
+     let nn = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+     }
+     setNotes(notes.concat(nn));
+     setNewNote('');
+     //console.log('nn ', nn.content) ;
+     }
+
+    const handleOnChange = (event) => {
+      console.log('onChange', event.target) ;
+      console.log('input value', event.target.value) ;
+      setNewNote(event.target.value);
+      console.log('newN', newNote) ;
     }
-  ]
 
-  return <Course courses={courses} />
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>     
+        <input value={newNote} onChange={handleOnChange}/>  
+        <button type="submit">save</button> 
+        </form> 
+    </div>
+  )
 }
 
 export default App;
+
+
