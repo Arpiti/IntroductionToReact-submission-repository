cd82173f51126e6a6d740d9208c03aed84add569
Arpit <arpit.rai@viayam.today>
2021-03-24 18:36:11 +0530

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react'
-let c = 1;
 
 const App = () => {
   
 
   const [ persons, setPersons ] = useState([{name: 'Arto Hellas'}]) ;
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('Text here')
+
+  const isSamePerson = (sum, person) => {
+    console.log('person name', person.name);
+    console.log('new name', newName);
+    if(person.name == newName)
+    {
+      console.log('in if condition');
+      sum =1;
+    }
+    return sum;
+  }
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log('Submit handled');
     
     const newPerson = {name: newName};
-    setPersons(persons.concat(newPerson));
+    const flagSum = persons.reduce(isSamePerson, 0)
+    let realPerson = [...persons];
+
+    console.log('flagsum= ', flagSum);
+
+    if(flagSum === 0)
+      realPerson = persons.concat(newPerson);
+    else
+       window.alert(`${newName} is already added to phonebook`);  
+    
+    console.log('real ', realPerson);
+    setPersons(realPerson);
     setNewName('');
  }
 
@@ -21,6 +42,7 @@ const App = () => {
     console.log('Input Change handled- ',event.target.value);
     setNewName(event.target.value);
   }
+
 
   return (
     <div>
