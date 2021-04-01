7420b84e539d83837bcb598755a6441c365e5405
Arpit <arpit.rai@viayam.today>
2021-04-01 23:52:34 +0530

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,63 @@
 import React, { useState } from 'react'
+let arr = [
+  { name: 'Arto Hellas', number: '040-123456' },
+  { name: 'Ada Lovelace', number: '39-44-5323523' },
+  { name: 'Dan Abramov', number: '12-43-234345' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122' }
+];
 
 const App = () => {
   
+  
+  const Filter = () => {
+    return (
+      <div>
+        filter shown with: <input type='text' onChange={handleFilterInputChange} value={filter}/>
+      </div>
+    )
+  }
 
-  const [ persons, setPersons ] = useState([{name: 'Arto Hellas', number: '040-1234567'}]) ;
+  const [persons, setPersons] = useState(arr)
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('') ;
+
+  let finArr = [...arr];
+
+  const handleFilterInputChange = (event) => {
+    console.log(event.target.value);
+
+    let val = event.target.value;
+    
+    
+    if(val=='' || val ==='underfined')
+    {
+      console.log('val',val);  
+      setFilter(val); 
+      console.log('finArr',finArr);  
+      setPersons(finArr);
+    }      
+    else
+    {
+      setFilter(val);     
+      var filt = persons.filter(person => {
+        console.log('filter',val);
+        if (((person.name).startsWith(val)))
+        {
+          return true;
+        }
+        return false;
+      })  
+      setPersons(filt);
+    }
+    
+
+    console.log('filt',filt);  
+    
+    
+
+  }
 
   const isSamePerson = (sum, person) => {
     console.log('person name', person.name);
@@ -27,6 +79,7 @@ const App = () => {
     const flagSum = persons.reduce(isSamePerson, 0)
     let realPerson = [...persons];
 
+
     console.log('flagsum= ', flagSum);
 
     if(flagSum === 0)
@@ -36,7 +89,7 @@ const App = () => {
     
     console.log('real ', realPerson);
     setPersons(realPerson);
-    setNewNumber()
+    setNewNumber('');
     setNewName('');
  }
 
@@ -52,11 +105,15 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input  onChange={handleFilterInputChange} value={filter}/>
+      </div>
+      <br/>
       <form onSubmit={handleFormSubmit}>
+      <h2>Add new</h2>
         <div>
           name: <input onChange={handleNameInputChange} value={newName}/> 
         </div>
@@ -69,10 +126,15 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person=> <li key={person.name}>{person.name} {person.number}</li>)}
+      {
+      persons.map(person=> <li key={person.name}>{person.name} {person.number}</li>)
+      //persons.filter(person => (((person.name).indexOf(filter))!=-1))    
+      } 
       </ul>
     </div>
   )
 }
+
+
 
 export default App
