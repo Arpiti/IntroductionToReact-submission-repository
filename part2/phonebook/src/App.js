5bc9cbe5427b6368d77b943e4f06d7fb911460b6
Arpit <arpit.rai@viayam.today>
2021-04-02 01:33:22 +0530

    using loacaleCompare to equate strings

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,49 +6,49 @@ let arr = [
   { name: 'Mary Poppendieck', number: '39-23-6423122' }
 ];
 
-const Filter = ({filter, handleFilterInputChange}) => {
+const Filter = ({ filter, handleFilterInputChange }) => {
   return (
     <div>
-      filter shown with: <input type='text' onChange={handleFilterInputChange} value={filter}/>
+      filter shown with: <input type='text' onChange={handleFilterInputChange} value={filter} />
     </div>
   )
 }
 
-const Persons = ({persons}) => {
+const Persons = ({ persons }) => {
   return (
     <ul>
-    {
-    persons.map(person=> <li key={person.name}>{person.name} {person.number}</li>)
-    //persons.filter(person => (((person.name).indexOf(filter))!=-1))    
-    } 
+      {
+        persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+        //persons.filter(person => (((person.name).indexOf(filter))!=-1))    
+      }
     </ul>
   )
 }
 
-const Form = ({handleFormSubmit, handleNameInputChange, newName, handleNumberInputChange, newNumber}) => {
+const Form = ({ handleFormSubmit, handleNameInputChange, newName, handleNumberInputChange, newNumber }) => {
 
-return (
-<form onSubmit={handleFormSubmit}>
-<div>
-  name: <input onChange={handleNameInputChange} value={newName}/> 
-</div>
-<div>
-  number: <input onChange={handleNumberInputChange} value={newNumber}/> 
-</div>
-<div>
-  <button type="submit">add</button>
-</div>
-</form>
-);
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        name: <input onChange={handleNameInputChange} value={newName} />
+      </div>
+      <div>
+        number: <input onChange={handleNumberInputChange} value={newNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
 }
 
 
 const App = () => {
   const [persons, setPersons] = useState(arr)
 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filter, setFilter ] = useState('') ;
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('');
 
   let finArr = [...arr];
 
@@ -57,30 +57,27 @@ const App = () => {
 
     event.preventDefault();
     let val = event.target.value;
-    
-    
-    if(val=='' || val ==='underfined')
-    {
-      console.log('val',val);  
-      setFilter(val); 
-      console.log('finArr',finArr);  
+
+
+    if (val == '' || val === 'underfined') {
+      console.log('val', val);
+      setFilter(val);
+      console.log('finArr', finArr);
       setPersons(finArr);
-    }      
-    else
-    {
-      setFilter(val);     
+    }
+    else {
+      setFilter(val);
       var filt = persons.filter(person => {
-        console.log('filter',val);
-        if (((person.name).startsWith(val)))
-        {
+        console.log('filter', val);
+        if (((person.name).startsWith(val))) {
           return true;
         }
         return false;
-      })  
+      })
       setPersons(filt);
     }
-    console.log('filt',filt);  
-    
+    console.log('filt', filt);
+
   }
 
   const isSamePerson = (sum, person) => {
@@ -88,9 +85,8 @@ const App = () => {
     console.log('new name', newName);
     console.log('new number', newNumber);
 
-    if(person.name == newName && person.number == newNumber)
-    {
-      sum =1;
+    if ((person.name).localeCompare(newName) && (person.number).localeCompare(newNumber)) {
+      sum = 1;
       console.log('same names bro >>');
     }
     return sum;
@@ -99,35 +95,34 @@ const App = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log('Submit handled');
-    
-    const newPerson = {name: newName, number: newNumber};
+
+    const newPerson = { name: newName, number: newNumber };
     const flagSum = persons.reduce(isSamePerson, 0)
     let realPerson = [...persons];
 
 
     console.log('flagsum= ', flagSum);
 
-    if(flagSum === 0)
-    { 
-       realPerson = persons.concat(newPerson);
-       arr = persons.concat(newPerson);
-    } 
-     else
-       window.alert(`${newName} is already added to phonebook`);  
-    
+    if (flagSum === 0) {
+      realPerson = persons.concat(newPerson);
+      arr = persons.concat(newPerson);
+    }
+    else
+      window.alert(`${newName} is already added to phonebook`);
+
     console.log('real ', realPerson);
     setPersons(realPerson);
     setNewNumber('');
     setNewName('');
- }
+  }
 
-  const handleNameInputChange = (event) => { 
+  const handleNameInputChange = (event) => {
     //event.preventDefault;
     console.log('Input Change handled- ', event.target.value);
     setNewName(event.target.value);
   }
 
-  const handleNumberInputChange = (event) => { 
+  const handleNumberInputChange = (event) => {
     //event.preventDefault;
     console.log('Input Change handled- ', event.target.value);
     setNewNumber(event.target.value);
@@ -136,13 +131,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-     <Filter filter={filter} handleFilterInputChange= {handleFilterInputChange}/>
-      <br/>
+      <Filter filter={filter} handleFilterInputChange={handleFilterInputChange} />
+      <br />
       <h2>Add new</h2>
       <Form handleFormSubmit={handleFormSubmit} handleNameInputChange={handleNameInputChange}
-       newName={newName} handleNumberInputChange={handleNumberInputChange} newNumber={newNumber}/>
+        newName={newName} handleNumberInputChange={handleNumberInputChange} newNumber={newNumber} />
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={persons} />
     </div>
   )
 }
