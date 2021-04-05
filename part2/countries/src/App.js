dd60cd06a2f339066eeed912d9f33c666cc22f48
Arpit <arpit.rai@viayam.today>
2021-04-05 13:59:52 +0530

    2.12* Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,6 +5,43 @@ import React, {useEffect, useState} from 'react';
 const allCountries_api = 'https://restcountries.eu/rest/v2/all';
 const singleCountryDetail_api = 'https://restcountries.eu/rest/v2/alpha/';
 
+function Result({resultCountries}) {
+  console.log('ResultCountries from Result >>', resultCountries);
+  if(resultCountries.length === 1 && resultCountries[0].alpha2Code != 'NA')
+  {
+    let country = resultCountries[0];
+    return (
+     <div>
+       <h2>{country.name}</h2>
+       <br/>
+       <p>Capital: {country.capital}</p> 
+       <p>Population: {country.population}</p> 
+       <br/>
+       <h3>language</h3>
+       <br/>
+       <ul>
+         {
+         (country.languages).map(lang => <li>{lang.name}</li>)
+        }
+       </ul>
+       <img src={country.flag} height='160px' width='200px'/>
+
+     </div> 
+    );
+  }
+  else {
+    
+    console.log('in Else return');
+    return (
+      <ul>
+         {
+           resultCountries.map(country => <li key={country.alpha2Code}> {country.name} </li>)
+         }
+       </ul>
+    );
+  }
+}
+
 function App() {
 
   const [inputCountry, setInputCountry] = useState('');
@@ -17,9 +54,6 @@ function App() {
     console.log('Value', val);
 
     setInputCountry(val);
-    
-   
-
     const isInputInCountry = thisCountry => {
   
       //console.log('InputCountry', inputCountry);
@@ -40,17 +74,19 @@ function App() {
 
       if(totalResCountries.length === allCountries.length)
         setResultCountries([]);
-      // else if(totalResCountries.length === 1)
-      // {
-        
-      // }
+      else if(totalResCountries.length === 1)
+      {
+        console.log('Only one country in result');   
+        setResultCountries(totalResCountries);  
+      }
       else if(totalResCountries.length < 10)
         setResultCountries(totalResCountries);
       else
-        setResultCountries([{name: 'Too many countries, please specify any other filter', alpha2Code: 'TMC'}])
+        setResultCountries([{name: 'Too many countries, please specify any other filter', alpha2Code: 'NA'}])
     }
     console.log('Total countries', allCountries.length);
   };
+
 
   useEffect(()=>{
     console.log('In UseEffect');
@@ -62,19 +98,31 @@ function App() {
       })
   },[])
 
+
   useEffect(()=>{
     console.log('In Single country UseEffect');
-    axios
-      .get(singleCountryDetail_api)
-      .then(response => {
-        console.log(' Single country UseEffect Promise Fulfilled');
-        console.log(response.data);
-        // setAllCountries(response.data);
-      })
-  },[])
+    console.log('resultCountries >>', resultCountries);
+    if(resultCountries.length === 1 && resultCountries[0].alpha2Code != 'NA')
+    {
+      let singleCountryCode = (resultCountries[0].alpha3Code);
+      console.log('SingleCountry >>', singleCountryCode);
+      console.log('In Single country UseEffect');
+
+      const uri = singleCountryDetail_api+singleCountryCode;
+      console.log('URI >>', uri);
+
+      axios
+       .get(uri)
+       .then(response => {
+          console.log(' Single country UseEffect Promise Fulfilled');
+          console.log(response.data);
+       })
+    }
+    },[inputCountry]);
+
 
   return (
-    <div className="App">
+    <div>
       <h2> Country Search App</h2>
       <br/>
      <div>
@@ -83,11 +131,7 @@ function App() {
      <div>
        <br/>
        <br/>
-       <ul>
-         {
-          resultCountries.map(country => <li key={country.alpha2Code}> {country.name} </li>)
-         }
-        </ul> 
+       <Result resultCountries={resultCountries}/>
      </div>
     </div>
   );
